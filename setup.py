63de932df28435bb1eb2e7c95749eb4fba6873e2
yilinxu <yellen10@gmail.com>
2020-05-22 12:57:13 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "gen3-augur-pyutils",
+    author = "Yilin Xu",
+    author_email = "yilinxu@uchicago.edu",
+    version = 0.1, 
+    description = "Utility tools for data accessing from gen3 and data processing with augur",
+    license = "Apache 2.0",
+    packages = find_packages(),
+    python_requires='>=3.5',
+    entry_points= ''' 
+        [console_scripts]
+        gen3-augur-pyutils=gen3-augur-pyutils.__main__:main
+    ''', 
+)
