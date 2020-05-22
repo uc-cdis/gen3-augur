42e06ad6f7bf881d896581bdd1532af3173c0e00
yilinxu <yellen10@gmail.com>
2020-05-22 15:31:38 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     description = "Utility tools for data accessing from gen3 and data processing with augur",
     license = "Apache 2.0",
     packages = find_packages(exclude=["*test*"]),
+    include_package_data=True,
     python_requires='>=3.5',
     entry_points= ''' 
         [console_scripts]
