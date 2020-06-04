2ab91dadb8c0482f3cbb5e3cea2b22660f0d7269
yilinxu <yellen10@gmail.com>
2020-06-04 10:26:26 -0500

    refactor

--- gen3_augur_pyutils/common/types.py
+++ gen3_augur_pyutils/common/types.py
@@ -4,11 +4,13 @@ Adapted from Kyle Hernandez's work (https://github.com/COV-IRT/dmwg-data-pyutils
 """
 from typing import NewType
 from argparse import ArgumentParser, Namespace
+from pandas import DataFrame
 from logging import Logger
 
-# ArgParser types and Namespace types
+# ArgParser types, Namespace types and DataFrame types
 ArgParserT = NewType("ArgParserT", ArgumentParser)
 NamespaceT = NewType("NamespaceT", Namespace)
+DataFrameT = NewType("DataFrameT", DataFrame)
 
 # Logger types
 LoggerT = NewType("LoggerT", Logger)
