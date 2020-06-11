b586091608d1f0f4f2dcd4d214fc930a95d72e69
yilinxu <yellen10@gmail.com>
2020-06-11 12:41:15 -0500

    add sample result

--- gen3_augur_pyutils/subcommands/augur_phylogeny.py
+++ gen3_augur_pyutils/subcommands/augur_phylogeny.py
@@ -1,1 +1,7 @@
-augur filter --sequences
+augur align --sequences covid19-0609.fasta --reference-sequence sequence.gb --output result/aligned.fasta --fill-gaps
+augur tree --alignment results/aligned.fasta --output results/tree_raw.nwk
+
+
+
+https://github.com/nextstrain/auspice/blob/master/cli/server/convertJsonSchemas.js #342
+
