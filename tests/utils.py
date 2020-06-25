84a80916cf0458973296bd15f98adf89e99da762
yilinxu <yellen10@gmail.com>
2020-06-25 11:35:24 -0500

    unit test

--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,10 @@
 """Utilities for testing."""
 import os
+import sys
+from contextlib import contextmanager
+from io import StringIO
+
+from gen3_augur_pyutils.common.logger import Logger
 
 
 def cleanup_files(files):
@@ -19,3 +24,16 @@ def cleanup_files(files):
 
     for fil in flist:
         _do_remove(fil)
+
+
+@contextmanager
+def capture_output():
+    """Captures stderr and stdout and returns them"""
+    new_out, new_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = new_out, new_err
+        Logger.setup_root_logger()
+        yield sys.stdout, sys.stderr
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err
