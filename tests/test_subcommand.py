41a5ba7b6f454d36a8a4663ae2d5130b278c76a2
yilinxu <yellen10@gmail.com>
2020-06-25 11:35:24 -0500

    unit test

---
+++ tests/test_subcommand.py
@@ -0,0 +1,38 @@
+"""Tests the gen3_augur_pyutils.subcommands.base module"""
+import unittest
+
+from utils import captured_output
+
+from gen3_augur_pyutils.__main__ import main
+from gen3_augur_pyutils.subcommands import Subcommand
+
+
+class TestSubcommand(unittest.TestCase):
+    class Example(Subcommand):
+        @classmethod
+        def __add_arguments__(cls, subparser):
+            pass
+
+        @classmethod
+        def __main__(cls, options):
+            pass
+
+        @classmethod
+        def __get_description__(cls):
+            return "Example description"
+
+    def test_get_name(self):
+        self.assertEqual(TestSubcommand.Example.__tool_name__(), "Example")
+        self.assertIsNone(Subcommand.__get_description__())
+
+    def test_no_inputs(self):
+        with capture_output() as (_, stderr):
+            with self.assertRaises(SystemExit) as context:
+                main(args=['Example'])
+        self.assertTrue("invalid choice: 'Example'" in stderr.getvalue())
+
+    def test_extra_subparser(self):
+        with captured_output() as (_, stderr):
+            with self.assertRaises(SystemExit) as context:
+                main(args=['Example', '--fake'], extra_subparser=TestSubcommand.Example)
+        self.assertTrue('unrecognized auguments: --fake' in stderr.getvalue())
