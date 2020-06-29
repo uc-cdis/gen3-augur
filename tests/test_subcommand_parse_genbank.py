fda8ed296f528b655f20d731499986faab04ee34
yilinxu <yellen10@gmail.com>
2020-06-29 10:17:22 -0500

    country region mapping

---
+++ tests/test_subcommand_parse_genbank.py
@@ -0,0 +1,7 @@
+"""Test gen3_augur_pyutils.subcommands.parse_genbank module"""
+import unittest
+from gen3_augur_pyutils.subcommands.parse_genbank import ParseGenBank
+
+class TestSubcommandParseGenbank(unittest.TestCase):
+   def test_parse_bg(self):
+
