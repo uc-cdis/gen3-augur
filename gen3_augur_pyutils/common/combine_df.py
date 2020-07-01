9269bedb924dee1bc3d56f831038408d27d9e243
yilinxu <yellen10@gmail.com>
2020-07-01 11:10:57 -0500

    refactor

--- gen3_augur_pyutils/common/combine_df.py
+++ gen3_augur_pyutils/common/combine_df.py
@@ -4,6 +4,16 @@
 
 
 def merge_multiple_columns(left_df, right_df, left_merge_column, right_merge_columns, right_target_column):
+    """
+    The target column of first dataframe codes information in multiple ways. The second dataframe has multiple columns, each column code the same information in one way.
+    This function allows the first dataframe merge on multiple columns from the second dataframe.
+    :param left_df: First dataframe
+    :param right_df: Second dataframe
+    :param left_merge_column: The column name on the first dataframe
+    :param right_merge_columns: Multiple columns names on the second dataframe
+    :param right_target_column: The additional information from second dataframe to be included in the merged dataframe
+    :return:
+    """
     for c_name in right_merge_columns:
         left_df = left_df.merge(right_df[[c_name, right_target_column]], how='left', left_on=left_merge_column,
                                 right_on=c_name).drop(c_name, axis='columns')
