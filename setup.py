7feb60e4ca7a7d6531ae59868bcd6569c0e3e534
Pauline <ribeyre@uchicago.edu>
2020-07-08 12:25:40 -0500

    fix imports for test_common_io

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
 
+
 setup(
     name="gen3-augur",
     author="Yilin Xu",
@@ -9,6 +10,10 @@ setup(
     description="Utility tools for data accessing from gen3 and data processing with augur",
     license="Apache 2.0",
     packages=find_packages(exclude=["*test*"]),
+    install_requires=[
+        "pandas==1.0.5",
+        "biopython==1.77"
+    ],
     package_data={'': ['config/country_region_mapper.csv']},
     include_package_data=True,
     python_requires='>=3.5',
